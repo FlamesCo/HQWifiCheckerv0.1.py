9a92e6b3bc585e3d89c0d8e8025788963bb87676
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-03-05 17:54:40 -0800

    Request to upload 2.5.22
    # Scanning

---
+++ SystemTest.java
@@ -0,0 +1,29 @@
+
+// TEST V1.0X
+// Language: java
+// Path: SystemTest.java
+
+public class SystemTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // write a int name age with the value 15
+        int age = 15;
+        System.out.println("My age is " + age);
+
+        // write a if else statement to check if the wifi is online
+        // if it is online, print "wifi is online"
+        // if it is not online, print "wifi is offline"
+        // write a md5 hash checker that checks your cookies if they are there
+
+        {
+            String cookie = "";
+            String hash = "";
+            if (cookie.equals(hash)) {
+                System.out.println("wifi is online");
+            } else {
+                System.out.println("wifi is offline");
+            }
+        }
+    }
+}
